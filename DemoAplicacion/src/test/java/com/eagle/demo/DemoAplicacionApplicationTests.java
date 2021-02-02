97fd931d8f8d09fdcd45799dfb8ed7d78f715539
ivan_ <ivan_@MONTES>
2021-02-01 18:03:50 -0600

    Comit inicial con eclipse. Close #1

---
+++ DemoAplicacion/src/test/java/com/eagle/demo/DemoAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
